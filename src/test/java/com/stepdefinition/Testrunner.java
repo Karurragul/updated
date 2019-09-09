a9019b876f0ed04b2367f1a965851eea4f9730ef
karurragul <karurragul@gmail.com>
2019-09-09 07:56:27 +0530

    success

--- src/test/java/com/stepdefinition/Testrunner.java
+++ src/test/java/com/stepdefinition/Testrunner.java
@@ -6,12 +6,12 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Features", glue = "com.stepdefinition", plugin = {"html:target","json:target\report.json"
-		,"rerun:src/test/resources/Features/fail.txt" }, dryRun = false, monochrome = true, strict = true)
+@CucumberOptions(features = "src/test/resources/Features/onedlist.feature", glue = "com.stepdefinition", plugin = {
+		"html:target", "json:target/report.json",
+		"rerun:src/test/resources/Features/fail.txt" }, dryRun = false, monochrome = true, strict = true)
 public class Testrunner {
 
 }
 
-
-//tags = { "@tag2" },
-//tags = { "@tag1,@tag2" },
+// tags = { "@tag2" },
+// tags = { "@tag1,@tag2" },
